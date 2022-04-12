7e18e2f373a320029223e800354a9689ca5cfa54
Kristy Guo <krisxxg@gmail.com>
2022-04-11 22:30:01 -0700

    pull params from api URL

--- assets/searchResult.js
+++ assets/searchResult.js
@@ -3,6 +3,8 @@ let recipeInput;
 let maxTimeInput;
 let dietInput;
 
+let resultsEl = document.querySelector(".results-div");
+
 //on page load, we execute the getParams() function
 let getParams = function() {
     //Get search params out of the URL
@@ -22,7 +24,7 @@ let searchAPI = function() {
     let spoonacularScAPI= "d50e45466d7749c6b088e5e791b622e8";
     let spoonacularSkAPI= "";
 
-    let complexSearchAPIUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${recipeInput}&maxReadyTime=${maxTimeInput}&diet=${dietInput}&num=6&apiKey=${spoonacularKgAPI}`;
+    let complexSearchAPIUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${recipeInput}&diet=${dietInput}&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&maxReadyTime=${maxTimeInput}&num=6&apiKey=${spoonacularKgAPI}`;
 
 
     fetch(complexSearchAPIUrl)
@@ -31,6 +33,42 @@ let searchAPI = function() {
     })
     .then(function(data){
         console.log(data);
+
+        for (let i=0;  i<7; i++) {
+            let recipeCard = document.createElement("div");
+
+            let recipeTile = document.createElement("h2");
+            recipeTile.textContent = data.results[i].title;
+            console.log(data.results[i].title);
+
+            let image = document.createElement("img");
+            image.setAttribute("href", data.results[i].image);
+
+            let readyTime = document.createElement("p");
+            readyTime.textContent = data.results[i].readyInMinutes;
+            console.log(data.results[i].readyInMinutes);
+
+            let summary = document.createElement("p");
+            summary.textContent = data.results[i].summary;
+            console.log(data.results[i].summary);
+
+            let sourceUrl = document.createElement("a");
+            sourceUrl.setAttribute("src", data.results[i].sourceUrl);
+            console.log(data.results[i].sourceUrl);
+
+
+            //append element to div
+            recipeCard.appendChild(recipeTile);
+            recipeCard.appendChild(image);
+            recipeCard.appendChild(readyTime);
+            recipeCard.appendChild(summary);
+            recipeCard.appendChild(sourceUrl);
+
+            //append recipeCard to results-div
+            resultsEl.appendChild(recipeCard);
+        
+
+        }
     })
 
 };
