df1609c58113980648b0cbc97b88eceff5259e0a
Kristy Guo <krisxxg@gmail.com>
2022-04-14 16:05:18 -0700

    animation add

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,25 @@ searchBtnEl.addEventListener("click", function(){
     maxTimeInput = maxReadyTimeInputEl.value.trim();
     dietInupt= dietInputEl.value.trim();
 
-    if (!recipeInput || !maxTimeInput || !dietInupt) {
+    if (!recipeInput) {
         console.error('You need a search input value!');
+        recipeInputEl.classList.add('animate__animated', 'animate__flash');
+        recipeInput.style.setProperty('--animate-duration', '0.5s');
+        return;
+
+    } else if (!maxTimeInput) {
+        console.error('You need a search input value!');
+        maxReadyTimeInputEl.classList.add('animate__animated', 'animate__flash');
+        maxReadyTimeInputEl.style.setProperty('--animate-duration', '0.5s');
+        return;
+
+    } else if (!dietInput) {
+        console.error('You need a search input value!');
+        dietInputEl.classList.add('animate__animated', 'animate__flash');
+        dietInputEl.style.setProperty('--animate-duration', '0.5s');
         return;
     }
+        
    
     var querySting = `./searchResult.html?q=${recipeInput}&maxreadytime=${maxTimeInput}&diet=${dietInupt}`;
     location.assign(querySting);
